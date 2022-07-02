a46200edda9263291308c3c4a45bb3ddb1045d97
sawaca96 <sawaca96@gmail.com>
2022-07-02 15:17:02 +0900

    🐛 Fix to use engine connection

--- app/main.py
+++ app/main.py
@@ -3,15 +3,13 @@ from typing import Dict
 import sqlalchemy as sa
 from fastapi import FastAPI
 from pydantic import EmailStr
-from sqlalchemy.engine.cursor import CursorResult
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from app.config import settings
 from app.tables import user
 
 app = FastAPI()
 engine: AsyncEngine = create_async_engine(settings.PG_DSN)
-session = AsyncSession(engine)
 
 
 @app.get("/ping")
@@ -21,11 +19,11 @@ def ping() -> str:
 
 @app.post("/users")
 async def create_user(username: str, email: EmailStr) -> Dict[str, int]:
-    async with session:
+    async with engine.begin() as conn:
         query = (
             sa.insert(user)
             .values({"username": username, "email": email})
             .returning(user.c.id)
         )
-        result: CursorResult = await session.execute(query)
+        result = await conn.execute(query)
     return {"id": result.one()[0]}
